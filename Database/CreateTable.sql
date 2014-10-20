a7ebbbbc35b8998c43430073f5f28171347c052e
Michael Hoppal <Michael_Hoppal@Michaels-MacBook-Air.local>
2014-10-19 23:25:27 -0600

    Adding database schema and scripts to create database

---
+++ Database/CreateTable.sql
@@ -0,0 +1,52 @@
+CREATE TABLE User (
+	userID int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	PRIMARY KEY (userID)
+);
+
+
+CREATE TABLE MenuItem (
+	menuItemID int NOT NULL AUTO_INCREMENT,
+	price float NOT NULL,
+	description varchar(255),
+	status ENUM('active', 'expired') NOT NULL,
+	PRIMARY KEY (menuItemID)
+);
+
+CREATE TABLE UserOrder (
+	orderID int NOT NULL AUTO_INCREMENT,
+	customerAddress varchar(255),
+        status ENUM('new', 'active', 'cancelled', 'complete'),
+	type ENUM('delivery', 'pickup', 'eatin'),
+	total float,	
+	userID int,
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	PRIMARY KEY (orderID)
+);
+
+CREATE TABLE OrderItem (
+	orderItemID int NOT NULL AUTO_INCREMENT,
+	quantity varchar(255) NOT NULL,
+	menuItemID int,
+	orderID int,
+	FOREIGN KEY (menuItemID) REFERENCES MenuItem(menuItemID),
+	FOREIGN KEY (orderID) REFERENCES UserOrder(orderID),
+	PRIMARY KEY (orderItemID)
+);
+
+
+CREATE TABLE DailySpecial (
+	dailySpecialID int NOT NULL AUTO_INCREMENT,
+	status ENUM('active', 'expired') NOT NULL,
+	PRIMARY KEY (dailySpecialID)
+);
+
+
+CREATE TABLE DailySpecialItem (
+	dailySpecialItemID int NOT NULL AUTO_INCREMENT,
+	dailySpecialID int,
+	menuItemID int,
+	FOREIGN KEY (menuItemID) REFERENCES MenuItem(menuItemID),
+        FOREIGN KEY (dailySpecialID) REFERENCES DailySpecial(dailySpecialID),
+        PRIMARY KEY (dailySpecialItemID)
+);
---
+++ Database/RemoveTable.sql
@@ -0,0 +1,8 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE if exists UserOrder;
+DROP TABLE if exists User;
+DROP TABLE if exists OrderItem;
+DROP TABLE if exists DailySpecialItem;
+DROP TABLE if exists DailySpecial;
+DROP TABLE if exists MenuItem;
+SET FOREIGN_KEY_CHECKS = 1;
